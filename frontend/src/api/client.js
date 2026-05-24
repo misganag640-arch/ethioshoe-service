040b676e32d1166006876ae600e012bbc78342a1
Misgana <misganag640.gmail.com>
2026-05-24 08:48:36 +0300

    Fix frontend api client

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,17 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
-});
-
-api.interceptors.request.use((c) => {
-  const t = localStorage.getItem('token');
-
-  if (t) {
-    c.headers.Authorization = `Bearer ${t}`;
-  }
-
-  return c;
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
